051acd445dbe45262ec6def1e930aa51e1ed6b3a
abhishekg2505 <abhishekg2505@gmail.com>
2025-09-18 17:59:52 +0530

    correct spelling

--- src/components/homepage/SubscribeBanner.tsx
+++ src/components/homepage/SubscribeBanner.tsx
@@ -9,7 +9,7 @@ const SubscribeBanner = () => {
     <div className="w-full bg-gradient-to-r from-[#08C1F0] via-[#8C8BFF] to-[#B560FF] py-14 px-4 md:px-10">
       <div className="flex flex-col md:flex-row items-center justify-between gap-4 max-w-7xl mx-auto">
         <h2 className="text-black text-h3 md:text-h2 font-antonio font-normal text-center md:text-left">
-          SUBSCRIBE NOW ON YOUR FAVORITE PLATFORM
+          SUBSCRIBE NOW ON YOUR FAVOURITE PLATFORM
         </h2>
         <div className="flex items-center gap-5 mt-2 md:mt-0">
           <Link href="https://www.youtube.com/@QuantumMindsPodcast" target="_blank">
